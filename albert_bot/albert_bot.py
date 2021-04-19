c6a9673d1dd006028a15d3cf8c8ffd309d512148
Fabien Lassagne <flassagne@unowhy.com>
2021-04-19 22:25:56 +0200

    ✨ pick a random human user between discord users

--- albert_bot/albert_bot.py
+++ albert_bot/albert_bot.py
@@ -15,11 +15,16 @@ async def on_ready():
 @bot.command(name="pick")
 async def pick_human_member(ctx):
     guild = discord.utils.get(bot.guilds, name='Cafyy')
-
-    humans = list(filter(lambda m: not m.bot, guild.members))
-    chosen = choice(humans)
+    chosen = pick_human(guild.members)
 
     await ctx.send(f"Je choisis {chosen.name}")
 
 def run():
     bot.run(os.environ.get('BOT_TOKEN'))
+
+def pick_human(members):
+    humans = list(filter(lambda m: not m.bot, members))
+    if not humans:
+        return None
+
+    return choice(humans)
--- test/test_albert_bot.py
+++ test/test_albert_bot.py
@@ -1,6 +1,20 @@
-from albert_bot.albert_bot import AlbertBot
+from collections import namedtuple
+from albert_bot.albert_bot import pick_human
+
+Member = namedtuple("Member", ['name', 'bot'])
+
+def test_pick_someone_between_humans() -> None:
+    members = [Member("albert", False), Member("josé", False)]
+
+    picked = pick_human(members)
+
+    assert not picked.bot
+    assert picked in members
 
 
-def test_bot_instantiate() -> None:
-    albert = AlbertBot()
-    assert albert.on_ready() == 'Logged as albert-bot!'
+def test_pick_any_between_bots() -> None:
+    members = [Member("r2d2", True), Member("hal9000", True)]
+
+    picked = pick_human(members)
+
+    assert picked is None
